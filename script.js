934479fc12cddef7d57a780ea72d9fc79d9d34f9
dnyord0x <149673453+dnyord0x@users.noreply.github.com>
2024-02-17 13:42:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $("#news-slider").owlCarousel({
+        items: 3,
+        margin: 15,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+});
